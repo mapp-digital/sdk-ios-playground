cdccab01c9506b4fb21769b0b151eb77402dcca7
Stefan Stevanovic <stefan.stevanovic@MRSFVFF92ZJQ05N.local>
2024-07-08 12:48:55 +0200

    initial commit

---
+++ sdk-ios-playground/sdk_ios_playground.h
@@ -0,0 +1,12 @@
+//
+//  sdk_ios_playground.h
+//  sdk-ios-playground
+//
+//  Created by Stefan Stevanovic on 8.7.24..
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sdk_ios_playground : NSObject
+
+@end
---
+++ sdk-ios-playground/sdk_ios_playground.m
@@ -0,0 +1,12 @@
+//
+//  sdk_ios_playground.m
+//  sdk-ios-playground
+//
+//  Created by Stefan Stevanovic on 8.7.24..
+//
+
+#import "sdk_ios_playground.h"
+
+@implementation sdk_ios_playground
+
+@end
